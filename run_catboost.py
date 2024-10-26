87fd79aefd4289ea8fbb5084cd405b5d9c831c08
Петров Александр <petrov1c@yandex.ru>
2024-10-26 18:42:00 +0300

    ...

--- run_catboost.py
+++ run_catboost.py
@@ -8,12 +8,14 @@ load_dotenv()
 VERSION = os.environ['VERSION']
 rmse = os.environ['rmse']
 
-TRAIN_COLUMNS = ['user', 'track', 'artist', 'pop', 'duration']
+TRAIN_COLUMNS = ['user', 'track', 'artist', 'genre', 'pop', 'duration']
 
 tracks = pd.read_json("data/tracks.json", lines=True)
 test_data = pd.read_csv("data/test.csv")
 test_data = pd.merge(test_data, tracks, how='left', on='track')
-test_pool = Pool(data=test_data[TRAIN_COLUMNS], cat_features=['user', 'track', 'artist'])
+test_data.genre = test_data.genre.map(lambda genres: genres[0])
+
+test_pool = Pool(data=test_data[TRAIN_COLUMNS], cat_features=['user', 'track', 'artist', 'genre'])
 
 model = CatBoostRegressor()
 model.load_model('catboost_model.dump')
